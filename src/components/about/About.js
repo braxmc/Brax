7f1c36a16e6475a7931158e4439d7a3d91ca53f7
Braxton McClellan <thebraxmc@gmail.com>
2020-10-18 23:44:57 -0600

    updates

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,9 +8,11 @@ const About = () => (
         <p>
           I’ve always loved tech, I just never thought I was capable. After realizing my sales days were numbered I decided it was time for a change. I had a friend who had just graduated, got a job, and loved it. After watching him and and talking to countless other graduates I used him as a referral to get into DevPoint Labs who is partnered with the University of Utah in Salt Lake City, UT. I'm keen and very passionate so I'm excited to see where this takes me.
         </p>
+        <br />
         <p>
           Outside of coding I'm an avid gamer, currently im playing fortnite and runescape, but I play most major titles and lots of old games. I love motorsports such as dirtbiking and Harley's, and also enjoy traditional sports like lacrosse and pickleball. I'm also very passionate about traveling.
         </p>
+        <br />
         <p>
         I will be receiving my Full-Stack Web Development certificate from DevPoint Labs on 10/31/2020.
         </p>
